ced425598b553f968c7f6361fe2ac9238b6cd2eb
Soulaiman, Abdullah A <aasoulaiman01@student.rtc.edu>
2019-05-14 18:58:09 -0700

    import requests and json, print simple info

--- main/main.py
+++ main/main.py
@@ -1,1 +1,9 @@
+import requests
+import json
 
+api_req = requests.get("https://api.coinmarketcap.com/v1/ticker/")
+
+api = json.loads(api_req.content)
+
+print(api[0]["symbol"])
+print(api[0]["price_usd"])
