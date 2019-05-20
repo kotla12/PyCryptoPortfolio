1d6fffb7e29037a63bb77c1ea8e4692af11176e2
Soulaiman, Abdullah A <aasoulaiman01@student.rtc.edu>
2019-05-20 10:09:41 -0700

    created coin.db, moved the coin dictionary into tbl.coin and added coins values using costume def

--- main/main.py
+++ main/main.py
@@ -1,7 +1,7 @@
 from tkinter import *
 import requests
 import json
-
+import sqlite3
 
 
 #Create Tk() instatne
@@ -9,6 +9,20 @@ pycrypto = Tk()
 pycrypto.title('Crypto Portfolio')
 pycrypto.iconbitmap("img/favicon.ico")
 updateImage = PhotoImage(file= "img/refresh.png").subsample(2,2)
+
+# database sectoin
+con = sqlite3.connect("coin.db")
+cursorObj = con.cursor()
+cursorObj.execute("CREATE TABLE IF NOT EXISTS coin(id INTEGER PRIMARY KEY, symbol TEXT, amount INTEGER, price REAL)")
+con.commit()
+#funciton to insert values:
+def insert_coin (id,symbol, amount, price):
+    cursorObj.execute("INSERT INTO coin VALUES(?,?,?,?)",(id,symbol, amount, price))
+    con.commit()
+
+#insert_coin(2,"BCH",100,420)
+#insert_coin(3,"LTC",75,25)
+#insert_coin(4,"XMR",10,100.05)
 # font color
 def font_color(amount):
     if amount >= 0:
@@ -24,37 +38,20 @@ def my_portfolio():
     api = json.loads(api_req.content)
     #Creating coins Dictionary:
 
-    coins = [{ # dictionary to store info on each coin
-        "symbol": "BTC",
-        "amount_owned": 2,
-        "price_per_coin": 4325.21 # hybothetical price purchased as
-        },
-        {
-         "symbol": "BCH",
-        "amount_owned": 100,
-        "price_per_coin": 300
-                 },
-        {
-         "symbol": "LTC",
-         "amount_owned": 75,
-         "price_per_coin": 25
-            },
-            {
-         "symbol": "XMR",
-         "amount_owned": 10,
-         "price_per_coin": 86.05
-            }]
+    cursorObj.execute("SELECT * FROM coin")
+    coins = cursorObj.fetchall()
+
     total_pl = 0
     coin_row = 1
     total_current_value = 0
 
     for i in range(0,100): # ge data from first 4 coins in API
         for coin in coins: #
-            if api[i]["symbol"] == coin["symbol"]:
-                total_paid = coin['amount_owned'] * coin['price_per_coin'] # coins owned * inital price purchased at
-                currnet_value = coin["amount_owned"] * float(api[i]["price_usd"]) # coins owned * current price
-                pl_percoin = float(api[i]["price_usd"]) - coin["price_per_coin"] # profit/loss current price - inital price
-                total_pl_coin = pl_percoin * coin["amount_owned"] # total profit/loss
+            if api[i]["symbol"] == coin[1]:
+                total_paid = coin[2] * coin[3] # coins owned * inital price purchased at
+                currnet_value = coin[2] * float(api[i]["price_usd"]) # coins owned * current price
+                pl_percoin = float(api[i]["price_usd"]) - coin[2] # profit/loss current price - inital price
+                total_pl_coin = pl_percoin * coin[2] # total profit/loss
                 total_pl = total_pl + total_pl_coin
                 total_current_value = total_current_value + currnet_value
 
@@ -62,8 +59,8 @@ def my_portfolio():
                 #print("Price- ${0:.2f}".format(float(api[i]["price_usd"])))#
                 #confert to float as api[i]['price_usd'] is a unicode and does
                 #not accept fomart modifier
-                #print("Number of Coin: ", coin["amount_owned"])
-                #print("Initial Price Purchased At: $", coin['price_per_coin'])
+                #print("Number of Coin: ", coin[2])
+                #print("Initial Price Purchased At: $", coin[3])
                 #print("Total Amount Paid: ",
                 #"${0:.2f}".format(float(total_paid)))
                 #print("Current Value: ",
@@ -78,19 +75,19 @@ def my_portfolio():
 
                 price = Label(pycrypto, text="${0:.2f}".format(float(api[i]["price_usd"])), bg="#F3F4F6", fg="black", font= "Lato 12 ", padx = "2", pady= "2",borderwidth=2, relief= "groove")
                 price.grid(row= coin_row, column = 1, sticky=N + S + E + W)
-    
-                no_coins = Label(pycrypto, text=coin["amount_owned"], bg="#F3F4F6", fg="black", font= "Lato 12 ", padx = "2", pady= "2",borderwidth=2, relief= "groove")
+
+                no_coins = Label(pycrypto, text=coin[2], bg="#F3F4F6", fg="black", font= "Lato 12 ", padx = "2", pady= "2",borderwidth=2, relief= "groove")
                 no_coins.grid(row= coin_row, column = 2, sticky=N + S + E + W)
-    
+
                 amount_paid = Label(pycrypto, text="${0:.2f}".format(float(total_paid)), bg="#F3F4F6", fg="black", font= "Lato 12 ", padx = "2", pady= "2",borderwidth=2, relief= "groove")
                 amount_paid.grid(row= coin_row, column = 3, sticky=N + S + E + W)
-    
+
                 currnet_val = Label(pycrypto, text="${0:.2f}".format(float(currnet_value)), bg="#F3F4F6", fg=font_color(float("{0:.2f}".format(float(currnet_value)))), font= "Lato 12 ", padx = "2", pady= "2",borderwidth=2, relief= "groove")
                 currnet_val.grid(row= coin_row, column = 4, sticky=N + S + E + W)
-    
+
                 pl_coin = Label(pycrypto, text="${0:.2f}".format(float(pl_percoin)), bg="#F3F4F6", fg=font_color(float("{0:.2f}".format(float(pl_percoin)))), font= "Lato 12 ", padx = "2", pady= "2",borderwidth=2, relief= "groove")
                 pl_coin.grid(row= coin_row, column = 5, sticky=N + S + E + W)
-    
+
                 totalpl = Label(pycrypto, text="${0:.2f}".format(float(total_pl_coin)), bg="#F3F4F6", fg=font_color(float("{0:.2f}".format(float(total_pl_coin)))), font= "Lato 12 ", padx = "2", pady= "2",borderwidth=2, relief= "groove")
                 totalpl.grid(row= coin_row, column = 6, sticky=N + S + E + W)
 
@@ -113,25 +110,25 @@ name.grid(row= 0, column = 0, sticky=N + S + E + W)
 
 price = Label(pycrypto, text="Price", bg="#1c7071", fg="white", font= "Lato 12 bold", padx = "5", pady= "5",borderwidth=2, relief= "groove")
 price.grid(row= 0, column = 1, sticky=N + S + E + W)
-    
+
 no_coins = Label(pycrypto, text="Coins Owned", bg="#1c7071", fg="white", font= "Lato 12 bold", padx = "5", pady= "5",borderwidth=2, relief= "groove")
 no_coins.grid(row= 0, column = 2, sticky=N + S + E + W)
-    
+
 amount_paid = Label(pycrypto, text="Total Amount Paid", bg="#1c7071", fg="white", font= "Lato 12 bold", padx = "5", pady= "5",borderwidth=2, relief= "groove")
 amount_paid.grid(row= 0, column = 3, sticky=N + S + E + W)
-    
+
 currnet_val = Label(pycrypto, text="Current Value", bg="#1c7071", fg="white", font= "Lato 12 bold", padx = "5", pady= "5",borderwidth=2, relief= "groove")
 currnet_val.grid(row= 0, column = 4, sticky=N + S + E + W)
-    
+
 pl_coin = Label(pycrypto, text="P/L Per Coin", bg="#1c7071", fg="white", font= "Lato 12 bold", padx = "5", pady= "5",borderwidth=2, relief= "groove")
 pl_coin.grid(row= 0, column = 5, sticky=N + S + E + W)
-    
+
 totalpl = Label(pycrypto, text="Total P/L With Coin", bg="#1c7071", fg="white", font= "Lato 12 bold", padx = "5", pady= "5",borderwidth=2, relief= "groove")
 totalpl.grid(row= 0, column = 6, sticky=N + S + E + W)
 
 my_portfolio()
-pycrypto.mainloop()   
+pycrypto.mainloop()
+cursorObj.close()
+con.close()
+
 print("Program Completed")
-
-
-    
